363700f8c8486cdf642a175b6d32928cc878c9b5
Your Name <you@example.com>
2024-10-11 16:32:31 +0100

    Initial commit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -30,53 +30,62 @@ final class ProduitController extends AbstractController
     #[Route(name: 'app_produit_index', methods: ['GET'])]
     public function index2(Request $request, ProduitRepository $produitRepository, EntityManagerInterface $entityManager): Response
     {
+        // Récupérer la liste des pays
         $paysList = $entityManager->getRepository(Pays::class)->findAll();
+
+        // Récupérer les paramètres de requête
         $codeProd = $request->query->get('codeprod');
         $libProd = $request->query->get('libprod');
         $pays = $request->query->get('pays');
 
+        // Construire la requête
         $queryBuilder = $produitRepository->createQueryBuilder('p')
             ->leftJoin('p.IDPAYS', 'pays')
             ->addSelect('pays');
 
+        // Filtrer par code produit si fourni
         if ($codeProd) {
             $queryBuilder->andWhere('p.SEQPROD LIKE :codeprod')
                 ->setParameter('codeprod', '%' . $codeProd . '%');
         }
 
+        // Filtrer par libellé produit si fourni
         if ($libProd) {
             $queryBuilder->andWhere('p.LIBPROD LIKE :libprod')
                 ->setParameter('libprod', '%' . $libProd . '%');
         }
 
+        // Filtrer par pays si fourni
         if ($pays) {
             $queryBuilder->andWhere('pays.IDPAYS = :pays')
                 ->setParameter('pays', $pays);
         }
-
+        // Exécuter la requête
         $query = $queryBuilder->getQuery();
-
-        // Debug: afficher la requête SQL
-        $sql = $query->getSQL(); // Obtenir la requête SQL
-//        var_dump($sql); // Afficher la requête SQL pour le débogage
-
+        // Debug: afficher la requête SQL (décommenter si nécessaire)
+        // $sql = $query->getSQL();
+        // var_dump($sql);
         try {
             $produits = $query->getArrayResult();
+
+            // Vérifier si des produits ont été trouvés
             if (empty($produits)) {
-                // Gérer le cas où aucun produit n'est trouvé
-                $produits = []; // ou un message pour informer qu'aucun produit n'a été trouvé
+                $produits = []; // Vous pouvez également ajouter un message informatif ici
             }
         } catch (\Exception $e) {
             // Gérer l'erreur (journalisation, message d'erreur, etc.)
+            // Vous pourriez utiliser un logger au lieu de var_dump
             var_dump($e->getMessage());
             die();
         }
 
+        // Rendre la vue
         return $this->render('produit/index.html.twig', [
             'produits' => $produits,
             'paysList' => $paysList,
         ]);
     }
+
 
 
 
